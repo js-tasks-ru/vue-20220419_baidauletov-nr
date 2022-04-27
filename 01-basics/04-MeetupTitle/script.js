862a10e24954d79a45834cca523aa3de4372ad9b
Baidauletov <baidauletov.nr@gmail.com>
2022-04-27 21:23:19 +0600

    MeetupTitle task solution

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,18 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const app = createApp({
+  data() {
+    return {
+      meetupTitle: '',
+      meetupId: '',
+    };
+  },
+  watch: {
+    async meetupId(newVal) {
+      this.meetupTitle = await fetchMeetupById(newVal);
+    },
+  },
+});
+
+app.mount('#app');
