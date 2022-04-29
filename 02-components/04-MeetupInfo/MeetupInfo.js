969f4da70ae3e74638004d7ce19d3fb31b366cf6
Baidauletov <baidauletov.nr@gmail.com>
2022-04-29 21:24:42 +0600

    MeetupInfo task solution

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -2,20 +2,43 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+    },
+    place: {
+      type: String,
+    },
+    date: {
+      type: Number,
+    },
+  },
+  computed: {
+    dateFormat() {
+      return new Date(this.date).toLocaleString(navigator.language, {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+      });
+    },
+    isoTime() {
+      return new Date(this.date).toISOString().split('T')[0];
+    },
+  },
 
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="isoTime">{{ dateFormat }}</time>
       </li>
     </ul>`,
 });
